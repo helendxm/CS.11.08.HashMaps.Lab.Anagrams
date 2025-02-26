fad21304c75d7dfc2624013ca40e1a1b5ba363a8
helendai <helend2027@student.cis.edu.hk>
2025-02-26 23:05:15 +0800

    done!

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Implement **three** methods:
 2) **ArrayList<String> mostFrequentAnagram(HashMap<String, ArrayList<String>> anagrams)**
    Given a **hashmap** that holds lists of words that are anagrams, return the **list that has the most words** (the list that contains the most amount of anagrams).
 3) **void printKeyValuePairs(HashMap<String, ArrayList<String>> anagrams)**
-   Given a **hashmap** that holds lists of words that are anagrams, **output all of the key value pairs**.
+   Given a **hashmap** that holds lists of words that are anagrams, **output all of the key value pairs**.[Scan 25 Feb 2025 at 6.37 PM.pdf](../../Downloads/Scan%2025%20Feb%202025%20at%206.37%20PM.pdf)
--- src/main/java/AnagramSolver.java
+++ src/main/java/AnagramSolver.java
@@ -1,8 +1,10 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.io.File;
+import java.util.*;
 
 public class AnagramSolver {
-
     private AnagramSolver() {};
 
     /**
@@ -11,8 +13,44 @@ public class AnagramSolver {
      * @param filename
      * @return
      */
-    public static HashMap<String, ArrayList<String>> anagrams(String filename) {
-        return null;
+    public static HashMap<String, ArrayList<String>> anagrams(String filename) throws FileNotFoundException {
+        HashMap<String, ArrayList<String>> anagramsHash = new HashMap<>();
+        Scanner scanner = new Scanner(new File(filename));
+        while (scanner.hasNextLine()){
+            String currentLine = scanner.nextLine().trim();
+            String sortedWord = sortString(currentLine);
+
+            //check sorted word already exists
+            ArrayList<String> anagramsList;
+            anagramsList = anagramsHash.get(sortedWord);
+
+            if (anagramsList == null){
+                anagramsList = new ArrayList<>();
+            }
+            anagramsList.add(currentLine);
+            anagramsHash.put(sortedWord, anagramsList);
+        }
+
+        scanner.close();
+        return anagramsHash;
+    }
+
+    public static String sortString(String original){
+        char[] temp = original.toCharArray();
+        Arrays.sort(temp);
+        String sorted = new String(temp);
+        return sorted;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        HashMap<String,ArrayList<String>> anagramHash;
+        anagramHash = anagrams("src/main/resources/simpler.txt");
+        System.out.println(anagramHash);
+
+        ArrayList<String> mostFreq = new ArrayList<>();
+        mostFreq = mostFrequentAnagram(anagramHash);
+        System.out.println(mostFreq);
+        printKeyValuePairs(anagramHash);
     }
 
     /**
@@ -22,7 +60,13 @@ public class AnagramSolver {
      * @return
      */
     public static ArrayList<String> mostFrequentAnagram(HashMap<String, ArrayList<String>> anagrams) {
-        return null;
+        ArrayList<String> mostFrequent = new ArrayList<>();
+        for (ArrayList<String> anagramList : anagrams.values()){
+            if (anagramList.size() > mostFrequent.size()){
+                mostFrequent = anagramList;
+            }
+        }
+        return mostFrequent;
     }
 
     /**
@@ -31,7 +75,9 @@ public class AnagramSolver {
      * @param anagrams
      */
     public static void printKeyValuePairs(HashMap<String, ArrayList<String>> anagrams) {
-
+        for (Map.Entry<String, ArrayList<String>> entry : anagrams.entrySet()){
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 
 }
